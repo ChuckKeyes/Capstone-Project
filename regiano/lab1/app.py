9c4c10426354bf9be9e236dea521a285491c0bde
l3gacy08 <regiano2000@hotmail.com>
2026-06-14 18:00:25 +0200

    initial commit regiano capstone project lab1

---
+++ regiano/lab1/app.py
@@ -0,0 +1,21 @@
+# imports the question function from quiz_name.py
+from quiz_game import question
+
+#name of the quiz
+name = "math_quiz"
+
+# asks for input from the player
+player = input("Enter your name: ")
+
+#this runs the question function in quiz_name.py
+result = question()
+
+# checks whether the answer is right or not?
+if result:
+    print("Correct!")
+    
+    file = open("results.txt", "w")
+    file.write(player + ": Answered the question correct!")
+
+else:
+    print("Incorrect!")
