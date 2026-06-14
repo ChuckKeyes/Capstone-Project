bcb5685714cf8082f598f57b6e47315ad7681aeb
Zeek Miller <228266305+millertyme314-oss@users.noreply.github.com>
2026-06-14 15:38:15 -0400

    Clean up merge conflict in python_script_execute.sh
    Removed merge conflict markers and invalid comments.

--- zeek/Lab1/python_script_execute.sh
+++ zeek/Lab1/python_script_execute.sh
@@ -17,9 +17,3 @@ elif [ "$choice" = "2" ]; then
 else
     echo "invalid selection..Piss Off!!"
 fi
-
-<<<<<<< HEAD
-# Mess-up-merge2
-=======
-# fix2?
->>>>>>> 15c8bf44eeb3e65e5a9f0c4b3edd010acd445c1f
